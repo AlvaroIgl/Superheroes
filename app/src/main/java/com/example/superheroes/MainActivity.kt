8cb2a87a1f045897a3deb1d6be406c53cacc7e8a
DAM205 <DAM205@DAM205>
2023-11-15 08:52:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -0,0 +1,119 @@
+package com.example.superheroes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.superheroes.models.Hero
+import com.example.superheroes.models.HeroesRepository.heroes
+import com.example.superheroes.ui.theme.SuperheroesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SuperheroesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HeroApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HeroApp() {
+    LazyColumn {
+        items(heroes) {
+            HeroItem(hero = it, modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small)))
+        }
+    }
+}
+@Composable
+fun HeroItem(
+    hero: Hero,
+    modifier: Modifier = Modifier
+) {
+    Card(modifier = modifier
+        .clip(MaterialTheme.shapes.medium)) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_small))
+        ) {
+            HeroIcon(hero.imageRes)
+            HeroInformation(hero.nameRes, hero.descriptionRes)
+        }
+    }
+}
+
+@Composable
+fun HeroIcon(
+    @DrawableRes dogIcon: Int,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        modifier = modifier
+            .size(dimensionResource(R.dimen.image_size))
+            .padding(dimensionResource(R.dimen.padding_small))
+            .clip(MaterialTheme.shapes.small),
+        contentScale = ContentScale.Crop,
+        painter = painterResource(dogIcon),
+
+        // Content Description is not needed here - image is decorative, and setting a null content
+        // description allows accessibility services to skip this element during navigation.
+
+        contentDescription = null
+    )
+}
+
+@Composable
+fun HeroInformation(
+    @StringRes dogName: Int,
+    dogAge: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = stringResource(dogName),
+            modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small))
+        )
+        Text(
+            text = stringResource(R.string.description1, dogAge),
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SuperheroesTheme {
+        Greeting("Android")
+    }
+}
